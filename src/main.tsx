82749f75943d87580eca4214e8362d80f6a66e94
Robb-Designs <rtlee@aggies.ncat.edu>
2026-05-04 14:47:36 -0400

    wire FilterProvider to Main, wrap App inside

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import { ThemeProvider } from './context/ThemeContext.tsx'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "./context/ThemeContext.tsx";
+import FilterProvider  from "./context/FilterContext.tsx";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <ThemeProvider>
-    <App />
+    <FilterProvider>
+      <App />
+    </FilterProvider>
   </ThemeProvider>,
-)
+);
