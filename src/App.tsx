980b010968fc09f8a8de80b3fb75117328de70e4
Robb-Designs <rtlee@aggies.ncat.edu>
2026-05-04 13:48:22 -0400

    add context imports for ThemeContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-
+import { useContext } from "react";
+import { ThemeContext } from "./context/ThemeContext";
 import './App.css'
 
 function App() {
+  // Access the theme context using the useContext hook. This allows me to get the current theme and the function to toggle it.
+  const context = useContext(ThemeContext);
 
   return (
     <>
