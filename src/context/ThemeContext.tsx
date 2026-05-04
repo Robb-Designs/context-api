fa2d3758846082d043482fd8549badd426cb7cfb
Robb-Designs <rtlee@aggies.ncat.edu>
2026-05-04 01:36:13 -0400

    import ReactNode and defined theme provider prop types

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, ReactNode } from "react";
 
 // Type definition for the theme context
 // This defines the shape of the context value, which includes the current theme and a function to toggle it.
@@ -7,6 +7,11 @@ type ThemeContextType = {
   toggleTheme: () => void;
 };
 
+type ThemeProviderProps = {
+  children: ReactNode;
+};
+
 // Create the ThemeContext with an initial value of undefined.
 //This allows us to provide a default value for the context, which can be used when a component takes the context without a provider.
 export const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
