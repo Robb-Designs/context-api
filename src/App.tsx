e987168b81ef16e40d03380ae13edb9b8b3602ed
Robb-Designs <rtlee@aggies.ncat.edu>
2026-05-04 13:51:08 -0400

    add conditional for context being undefined with Error message

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ import './App.css'
 
 function App() {
   // Access the theme context using the useContext hook. This allows me to get the current theme and the function to toggle it.
+  // this variable will hold the value of the theme context, which includes the current theme and the toggle function. I can use this variable to apply styles or functionality based on the current theme.
   const context = useContext(ThemeContext);
+
+  // If the context is undefined, it means that the component is not wrapped in a ThemeProvider. In this case, we throw an error to inform the developer that they need to wrap their component tree with a ThemeProvider to use the theme context.
+  if (!context) {
+    throw new Error("ThemeContext must be used within a ThemeProvider");
+  }
 
   return (
     <>
