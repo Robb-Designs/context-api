760aeb189bc2db38cb5d25c62dcf966677003b9b
Robb-Designs <rtlee@aggies.ncat.edu>
2026-05-04 13:56:33 -0400

    create component ui  that displays the current theme and includes a button to toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useContext } from "react";
 import { ThemeContext } from "./context/ThemeContext";
-import './App.css'
+import "./App.css";
 
 function App() {
   // Access the theme context using the useContext hook. This allows me to get the current theme and the function to toggle it.
   // this variable will hold the value of the theme context, which includes the current theme and the toggle function. I can use this variable to apply styles or functionality based on the current theme.
   const context = useContext(ThemeContext);
-  // Destructure the theme and toggleTheme from the context. 
-  const {theme, toggleTheme} = context;
+  // Destructure the theme and toggleTheme from the context.
+  const { theme, toggleTheme } = context;
 
   // If the context is undefined, it means that the component is not wrapped in a ThemeProvider. In this case, we throw an error to inform the developer that they need to wrap their component tree with a ThemeProvider to use the theme context.
   if (!context) {
@@ -16,9 +16,12 @@ function App() {
 
   return (
     <>
-   
+      <div>
+        <h1>Theme: { theme }</h1>
+        <button onClick={ toggleTheme }>Toggle Theme</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
