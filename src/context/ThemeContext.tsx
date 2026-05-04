8c57f00b906b1bcc5578f33d3dc401871b66a8ff
Robb-Designs <rtlee@aggies.ncat.edu>
2026-05-04 01:31:00 -0400

    Refactored ThemeContextType theme type definition

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -3,6 +3,10 @@ import { createContext, useState } from "react";
 // Type definition for the theme context
 // This defines the shape of the context value, which includes the current theme and a function to toggle it.
 type ThemeContextType = {
-  theme: string;
+  theme: "light" | "dark"; 
   toggleTheme: () => void;
 };
+
+// Create the ThemeContext with an initial value of undefined.
+//This allows us to provide a default value for the context, which can be used when a component takes the context without a provider.
+export const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
